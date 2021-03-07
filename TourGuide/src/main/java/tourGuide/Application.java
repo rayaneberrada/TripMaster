2dd341052993f5b2af54c18711fcb812269f2cc2
rayaneberrada <rayaneberrada44500@gmail.com>
2021-03-07 23:06:20 +0100

    gpsClient and TourGuide microservices configured to be added to eureka server

--- TourGuide/src/main/java/tourGuide/Application.java
+++ TourGuide/src/main/java/tourGuide/Application.java
@@ -2,10 +2,12 @@ package tourGuide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
---
+++ TourGuideClient/src/test/java/fr/openclassrooms/rayane/tourGuide/TestRewardsService.java
@@ -0,0 +1,76 @@
+package fr.openclassrooms.rayane.tourGuide;
+
+import gpsUtil.GpsUtil;
+import gpsUtil.location.Attraction;
+import gpsUtil.location.VisitedLocation;
+import org.junit.Before;
+import org.junit.Test;
+import rewardCentral.RewardCentral;
+import tourGuide.helper.InternalTestHelper;
+import tourGuide.service.RewardsService;
+import tourGuide.service.TourGuideService;
+import tourGuide.user.User;
+import tourGuide.user.UserReward;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.*;
+
+public class TestRewardsService {
+  @Before
+  public void setUp() {
+    Locale.setDefault(new Locale("en", "US", "WIN"));
+  }
+
+  @Test
+  public void userGetRewards() throws ExecutionException, InterruptedException {
+    GpsUtil gpsUtil = new GpsUtil();
+    RewardCentral rewardCentral = new RewardCentral();
+    RewardsService rewardsService = new RewardsService(gpsUtil, rewardCentral);
+
+    InternalTestHelper.setInternalUserNumber(0);
+    TourGuideService tourGuideService =
+        new TourGuideService(gpsUtil, rewardsService, rewardCentral);
+
+    User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
+    Attraction attraction = gpsUtil.getAttractions().get(0);
+    user.addToVisitedLocations(new VisitedLocation(user.getUserId(), attraction, new Date()));
+    tourGuideService.trackUserLocation(user);
+    rewardsService.calculateRewards(user);
+    HashMap<String, UserReward> userRewards = user.getUserRewards();
+    tourGuideService.tracker.stopTracking();
+    assertTrue(userRewards.size() == 1);
+  }
+
+  @Test
+  public void isWithinAttractionProximity() {
+    GpsUtil gpsUtil = new GpsUtil();
+    RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
+    Attraction attraction = gpsUtil.getAttractions().get(0);
+    assertTrue(rewardsService.isWithinAttractionProximity(attraction, attraction));
+  }
+
+  @Test
+  public void nearAllAttractions() throws ExecutionException, InterruptedException {
+    GpsUtil gpsUtil = new GpsUtil();
+    RewardCentral rewardCentral = new RewardCentral();
+    RewardsService rewardsService = new RewardsService(gpsUtil, rewardCentral);
+    rewardsService.setProximityBuffer(Integer.MAX_VALUE);
+
+    InternalTestHelper.setInternalUserNumber(1);
+    TourGuideService tourGuideService =
+        new TourGuideService(gpsUtil, rewardsService, rewardCentral);
+
+    rewardsService.setProximityBuffer(10000);
+    rewardsService.calculateRewards(tourGuideService.getAllUsers().get(0));
+    HashMap<String, UserReward> userRewards =
+        tourGuideService.getUserRewards(tourGuideService.getAllUsers().get(0));
+    tourGuideService.tracker.stopTracking();
+
+    assertEquals(gpsUtil.getAttractions().size(), userRewards.size());
+  }
+}
